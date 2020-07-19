ff64ffa057b099c05676f3619b4c0052f2c06873
mustafaokta <mustafaokta@hotmail.com>
2020-07-19 19:52:04 +0300

    Rs'den gelen verilerle customer pojo sundan bean oluşturduk ve ekrana yazdık.

--- demo/src/main/java/com/btk/spring/demo/AppMain.java
+++ demo/src/main/java/com/btk/spring/demo/AppMain.java
@@ -1,11 +1,15 @@
 package com.btk.spring.demo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.util.Assert;
 @SpringBootApplication
 public class AppMain implements CommandLineRunner{
@@ -22,5 +26,23 @@ JdbcTemplate jdbcTemplate;
 public void run(String... args) throws Exception {
 	// TODO Auto-generated method stub
 	Assert.notNull(jdbcTemplate, "App-context sorunlu, jdbc template mevcut deðil");
+Long rowCount=	jdbcTemplate.queryForObject("select count(*) from customers", Long.class);
+System.out.println("Row count "+rowCount); 
+Assert.isTrue(rowCount==4, "Problem for row count");
+
+         List<Customer> customerList= jdbcTemplate.query("select * from customers", new RowMapper<Customer>() {
+
+	@Override
+	public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+		// TODO Auto-generated method stub
+		return new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name") );
+	}	
+});
+//þimdi deðerleri ekrana basalým
+for(Customer customer : customerList) {
+	System.out.println(customer.toString());
+	
+}
+
 }
 }
